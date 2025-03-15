8e88e8912df80b04cd23700d8e631a4e7c67dbe3
Nooria <samihamaysoonnooria@gmail.com>
2025-03-15 21:12:58 +0600

    first commit

---
+++ tuples operations.py
@@ -0,0 +1,12 @@
+tuplex = (False, "YoYo", 3.5, 68)
+
+print(type(tuplex))
+print(tuplex)
+print(tuplex[2])
+
+tuplex = tuplex + ("Saifan", "Tashdeed")
+print(tuplex)
+
+tuple1 = (50, 10, 60, 70, 50)
+
+print(tuple1.count(50))
